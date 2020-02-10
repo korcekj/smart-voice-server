c72bb3686f84285e2c0f7e36ca2629a16c876bb2
Jan Korcek <korcekus123@gmail.com>
2020-02-10 20:27:53 +0100

    Arduino sketch updated.

--- projects/Server/Server.ino
+++ projects/Server/Server.ino
@@ -1,35 +1,11 @@
 // ESP8266 SERVER LIBRARY
 #include <ESP8266_Server.h>
 
-// LED LIBRARY
-#include <Led.h>
-
-// LED STATUS
-Led strip = Led();
-int ledstatus = 0; // status of led (ON/OFF)
-
 // SERVER INITIALIZING
 ESP8266WebServer webserver(80);
 ESP8266_Server server = ESP8266_Server(&webserver);
 
 const int led = 13;
-
-//void handleNotFound() {
-//  digitalWrite(led, 1);
-//  String message = "File Not Found\n\n";
-//  message += "URI: ";
-//  message += server.uri();
-//  message += "\nMethod: ";
-//  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-//  message += "\nArguments: ";
-//  message += server.args();
-//  message += "\n";
-//  for (uint8_t i = 0; i < server.args(); i++) {
-//    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-//  }
-//  server.send(404, "text/plain", message);
-//  digitalWrite(led, 0);
-//}
 
 void setup(void) {
   pinMode(led, OUTPUT);
@@ -48,45 +24,13 @@ void setup(void) {
   Serial.println(server.getUrl());
   
   server.handleRoot();
+  server.handleLed();
   server.handleNotFound();
 
   // STRIP BEGIN
   strip.init();
-
-//  server.on("/turn-on", HTTP_POST, []() {
-//    server.send(200, "text/plain", "Led's were turned on.");
-//    
-//    // set value
-//    if (!Firebase.setInt(firebaseData, "/led/status", 1)) {
-//        Serial.print("setting /led/status failed:");
-//        Serial.println(firebaseData.errorReason());  
-//        return;
-//    }
-//    ledstatus = 1;
-//  });
-
-//  server.on("/turn-off", []() {
-//    server.send(200, "text/plain", "Led's were turned off.");
-//    
-//    // set value
-//    if (!Firebase.setInt(firebaseData, "/led/status", 0)) {
-//        Serial.print("setting /led failed:");
-//        Serial.println(firebaseData.errorReason());  
-//        return;
-//    }
-//    ledstatus = 0;
-//  });
-
-//  server.onNotFound(handleNotFound);
 }
 
 void loop(void) {
   server.update();
-
-//  if (ledstatus) {
-//    strip.on(0, 0, 255, 6, 100);
-//  }
-//  else {
-//    strip.off();
-//  }
 }
